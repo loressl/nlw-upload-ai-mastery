911ec328df5398d94f26afe41d9284ec6fc64e93
loressl <loryssl@hotmail.com>
2023-09-13 15:23:21 -0300

    chore: class 2 completed

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -2,6 +2,8 @@ import { fastify } from 'fastify'
 import { fastifyCors } from '@fastify/cors'
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from './routes/upload-video';
+import { createTranscriptionRoute } from './routes/create-transcription';
+import { generateAiCompletionRoute } from './routes/generate-ai-completion';
 
 const app = fastify()
 
@@ -11,6 +13,8 @@ app.register(fastifyCors, {
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAiCompletionRoute)
 
 app.listen({
     port: 3333,
